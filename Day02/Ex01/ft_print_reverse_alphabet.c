50f8dad410a2fe7585eb010eb5663d3d5532b6b0
robert ugarte <robertu@roberts-MBP.lan1>
2020-02-18 15:27:04 -0600

    ft_print_reverse_alphabet.c

---
+++ Day02/Ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+  write(1, &c, 1);
+  return (0);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+  char ch;
+  ch = 'z';
+  while (ch >= 'a')
+    {
+      ft_putchar(ch);
+      ch--;
+    }
+}
+
+int main()
+{
+  ft_print_reverse_alphabet();
+}
