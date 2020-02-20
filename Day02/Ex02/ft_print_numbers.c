f15dc5760f438b5afc2f60d690465285d850e727
robert ugarte <robertu@roberts-MBP.lan1>
2020-02-20 10:42:32 -0600

    ft_print_numbers.c

---
+++ Day02/Ex02/ft_print_numbers.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void ft_putchar(char a)
+{
+  write(1, &a, 1);
+  return;
+}
+
+void ft_print_numbers(void)
+{
+  char ch;
+  ch = '1';
+  while(ch <= '9')
+    {
+      ft_putchar(ch);
+      ch++;
+    }
+}
+
+int main(void)
+{
+  ft_print_numbers(void); 
+}
