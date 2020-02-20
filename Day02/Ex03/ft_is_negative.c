3c6deb4d384ec08ed2d58dac7a694c0991e5fe2d
robert ugarte <robertu@roberts-MBP.lan1>
2020-02-20 14:55:09 -0600

    ft_is_negative.c

--- Day02/Ex03/ft_is_negative.c
+++ Day02/Ex03/ft_is_negative.c
@@ -11,11 +11,11 @@ void ft_is_negative(int n)
   char ch;
   if(n >= 0);
     {
-      ft_putchar("P");
+      ft_putchar('P', n);
     }
     else(n < 0);
     {
-      ft_putchar("N");
+      ft_putchar('N', n);
     }
 }
 
