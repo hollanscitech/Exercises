e7a5f6c3a65efa9f99f548ef46313aaac516fe43
robert ugarte <robertu@roberts-MBP.lan1>
2020-02-20 12:53:10 -0600

    ft_is_negative.c

---
+++ Day02/Ex03/ft_is_negative.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write(1, &c, 1)
+  return;
+}
+
+void ft_is_negative(void)
+{
+  char ch;
+  ch = "N", "P";
+  while(ch = n)
+    {
+      ft_putchar("P");
+    }
+  while(ch = -n)
+    {
+      ft_putchar("N")
+    }
+}
+
+void ft_is_negative(int n)
+	{
+	  n >= 0;
+	  -n < 0;
+	}
+
+int main (void)
+{
+  ft_is_negative(int n);
+  return (0);
+}
