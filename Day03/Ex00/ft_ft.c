86eaea441c8556aa73e44f87906ad553952233ba
robert ugarte <robertu@roberts-MBP.lan1>
2020-02-21 12:26:55 -0600

    ft_ft.c

--- Day03/Ex00/ft_ft.c
+++ Day03/Ex00/ft_ft.c
@@ -1,17 +1,13 @@
 #include <unistd.h>
 #include<stdio.h>
 
-void ft_ft(int *nbr)
-{
-  int x;
-  nbr = &x;
-  {
-    printf("%d\n", *nbr);
-  }
-}
+void ft_ft(int *nbr);
 
 int main(void)
 {
+  int x;
+  nbr = &n;
   x = 42;
-  ft_ft(int x);
+  *nbr = 42;
+  ft_ft();
 }
