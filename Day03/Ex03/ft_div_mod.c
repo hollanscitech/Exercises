ce31f43a3e957b06fb1d3c80902ccb1d10763ba4
robert ugarte <robertu@roberts-MBP.lan1>
2020-02-25 10:15:27 -0600

    ft_div_mod.c

--- Day03/Ex03/ft_div_mod.c
+++ Day03/Ex03/ft_div_mod.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
-void ft_div_mod(int a, int b, int *div, int *mod) 
+void ft_div_mod(int a, int b, int *div, int *mod)
 {
-  
+  *div = a/b;
+  *mod = a/b;
+}
+
+int main(void)
+{
+  int c = 7/3;
+  float d = 0.00;
+  d = (float)7/3;
+  ft_div_mod(&c);
+  printf("%d\n", c);
+  ft_div_mod(&d);
+  printf("%.2f\n", d-floor(d));
 }
