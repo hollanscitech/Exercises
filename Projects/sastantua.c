68c097dabfbc1c00f0ab803d866e5a545b6c2366
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-03 21:12:24 -0600

    sastantua.c

---
+++ Projects/sastantua.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void ft_putchar(char a)
+{
+  write(1, &a, 1);
+}
+
+void sastantua(int size)
+{
+  int y = 0;
+  int x = 0;
+  while (y < size)
+    { 
+      x = 0;
+      while (x < size)
+	{
+	  ft_putchar(47);
+	  x++;
+          while (x < size)
+	    {
+	      ft_putchar(42);
+	      x++;
+	      while (x < size)
+		{
+		  ft_putchar(92);
+		  x++;
+		}
+	    }
+      	}
+      ft_putchar('\n');
+      y++;
+    }
+}
+
+int main(void)
+{
+  sastantua(5);
+  return 0;
+}
