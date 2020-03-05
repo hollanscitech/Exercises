45e58dc8ac166736c8083a4c09a3967e7cfcc024
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-05 12:08:50 -0600

    sastantua.c

--- Projects/sastantua.c
+++ Projects/sastantua.c
@@ -1,27 +1,34 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_putchar(char a)
 {
   write(1, &a, 1);
 }
 
+int ftGetHeight(int size)
+{
+  int height;
+  int counter;
+  height = 0;
+  counter = 1;
+  while (counter <= size)
+    {
+      height = counter + height + 2;
+      counter++;
+    }
+  return height;
+}
+
 void sastantua(int size)
 {
-  int x = 0;
-  int y = 0;
-  size = (size) * (size) + 1;
-  while (y < size)
-    {
-      ft_putchar(36);
-      x++;
-      y++;
-    }
-  ft_putchar('\n');
-  x++;
+  int height = ftGetHeight(size);
+  printf("%d", height);
 }
 
 int main(void)
 {
-  sastantua(3);
+  sastantua(5);
   return 0;
 }
+
