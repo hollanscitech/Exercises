269f98f2a27496f400d28ebbea78620bc0356bd5
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-06 12:19:52 -0600

    sastantua.c

--- Projects/sastantua.c
+++ Projects/sastantua.c
@@ -20,15 +20,72 @@ int ftGetHeight(int size)
   return height;
 }
 
+int ft_GetWidth(int size)
+{
+  int height;
+  int width;
+  int counter;
+  height = 3;
+  width = 7;
+  counter = 1;
+  while (counter < size)
+	{
+	  width = width + 6 + (height * 2);
+	  counter++;
+	  height++;
+	}
+  return width;
+}
+
 void sastantua(int size)
 {
   int height = ftGetHeight(size);
   printf("%d", height);
+  printf("\n");
+  int width = ft_GetWidth(size);
+  printf("%d", width);
+  printf("\n");
 }
 
 int main(void)
 {
-  sastantua(5);
+  sastantua(3);
   return 0;
 }
 
+/*
+if size = 1
+height = 3
+width = 7
+
+if size = 2
+height = 7
+width = 19
+
+if size = 3
+height = 12
+width = 33
+
+if size = 4 
+height = 18
+width = 51
+
+if size = 5
+height = 25
+width = 71
+
+if size = 6
+height = 33
+width = ??
+
+add 6 to previous width
+start loop, adding two for every digit after the previous height
+adding 6 only twice.
+
+add 8 to previous width
+start loop, adding two for every digit after the previous height
+adding 8 only twice
+
+.
+
+*/
