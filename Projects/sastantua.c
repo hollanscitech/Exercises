205fc5739c3895a6fd288482f153c236bcff0815
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-10 11:18:21 -0500

    sastantua.c

--- Projects/sastantua.c
+++ Projects/sastantua.c
@@ -6,7 +6,7 @@ void ft_putchar(char a)
   write(1, &a, 1);
 }
 
-int ftGetHeight(int size)
+int ft_GetHeight(int size)
 {
   int height;
   int counter;
@@ -22,86 +22,56 @@ int ftGetHeight(int size)
 
 int ft_GetWidth(int size)
 {
-  int height;
   int width;
   int counter;
-  height = 3;
+  int difference;
+  if (size == 0)
+    {
+      return (0);
+    }
   width = 7;
   counter = 1;
+  difference = 12;
   while (counter < size)
     {
-      width = width + 6 + (height * 2);
-      counter++;
-      height++;
-      if(counter % width >= 4)
+      width += difference;
+      if (counter % 2 == 1)
 	{
-	  width = width + 6 + (height * 2);
-	  counter++;
-	  height++;
+	  difference += 2;
 	}
+      else
+	{
+	  difference += (counter + 2);
+	}
+      counter++;
     }
   return width;
 }
 
 void sastantua(int size)
 {
-  int height = ftGetHeight(size);
-  printf("%d", height);
-  printf("\n");
+  int height = ft_GetHeight(size);
   int width = ft_GetWidth(size);
-  printf("%d", width);
-  printf("\n");
+  int y = 0;
+  int x = 0;
+  while (y < height)
+    {
+      x = 0;
+    while (x < width)
+      {
+        ft_putchar(42);
+        x++;
+    }
+    ft_putchar('\n');
+    y++;
+  }
+
 }
 
 int main(void)
 {
-  sastantua(0);
-  printf("\n");
-  sastantua(1);
-  printf("\n");
   sastantua(2);
-  printf("\n");
-  sastantua(3);
-  printf("\n");
-  sastantua(4);
-  printf("\n");
-  sastantua(5);
   return 0;
 }
 
-/*
-if size = 1
-height = 3
-width = 7
 
-if size = 2
-height = 7
-width = 19
-
-if size = 3
-height = 12
-width = 33
-
-if size = 4 
-height = 18
-width = 51
-
-if size = 5
-height = 25
-width = 71
-
-if size = 6
-height = 33
-width = ??
-
-add 6 to previous width
-start loop, adding two for every digit after the previous height
-adding 6 only twice.
-
-add 8 to previous width
-start loop, adding two for every digit after the previous height
-adding 8 only twice
-
-.
-
-*/
