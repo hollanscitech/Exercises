f2a7e16b269a95df2e4269248d3c30beb627fd58
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-23 17:36:35 -0500

    ft_recursive_factorial.c

--- Day04/Ex01/ft_recursive_factorial.c
+++ Day04/Ex01/ft_recursive_factorial.c
@@ -4,14 +4,15 @@ int ft_recursive_factorial(int nb)
 {
   if (nb == 0)
     return 1;
-  return nb * ft_recursive_factorial(nb - 1);
+  else
+    return nb * ft_recursive_factorial(nb - 1);
 }
 
 int main(void)
 {
   int x;
-  x = ft_recursive_factorial(5);
-  printf("%d\n", x);
+  x = 5;
+  printf("%d\n", ft_recursive_factorial(x));
   return 0;
 }
 
