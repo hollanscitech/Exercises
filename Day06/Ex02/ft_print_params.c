a26be3cc42d17f8d2f576ea6afe97f1b1600f9da
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-28 02:36:54 -0500

    ft_print_params.c

---
+++ Day06/Ex02/ft_print_params.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+  int i = 0;
+  while (str[i] != '\0')
+    {
+      ft_putchar(str[i]);
+      i++;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+  int i = 1;
+  while(i < argc)
+    {
+      ft_putstr(argv[i]);
+      i++;
+      ft_putchar('\n');
+    }
+  return 0;
+}
