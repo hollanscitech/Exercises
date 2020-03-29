8e022db17669cab5a75bb58e13515f6a8bff4c48
robert ugarte <robertu@roberts-MBP.lan1>
2020-03-28 20:32:55 -0500

    ft_strcpy.c

--- Day05/Ex03/ft_strcpy.c
+++ Day05/Ex03/ft_strcpy.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
-char *ft_strcpy(char *destination, char *source)
+char *ft_strcpy(char *dst, char *src)
 {
-  if (destination == 0)
+  int i;
+  i = 0;
+  while (src[i] != '\0')
     {
-      return 0;
+      dst[i] = src[i];
+      i++;
     }
-  char *ptr = destination;
-  while (*source != '\0')
-    {
-      *destination = *source;
-      destination++;
-      source++;
-    }
-  *destination = '\0';
-  return ptr;
+  dst[i] = '\0';
+  return dst;
 }
 
 int main(void)
 {
-  char source[] = "Hello";
-  char destination[0];
-  printf("%s\n", ft_strcpy(destination, source));
-  return (0);
+  char src[] = "hello";
+  char dst[0];
+  ft_strcpy(dst, src);
+  printf("%s\n", dst);
+  return 0;
 }
+
+/*
+int main(void)
+{
+  char src[] = "hello";
+  printf("src:%s\n", src);
+  char dst[0];
+  printf("src: %s\ndst:%s\n", src, dst);
+  ft_strcpy(dst, src);
+  printf("src:%s\ndst:%s\n", src, dst);
+  return 0;
+}
+*/
+
 
