2b2672056557d4cdb2e6c67ff12e507cb1507575
robert ugarte <robertu@roberts-MBP.lan1>
2020-04-04 12:33:01 -0500

    ft_strdup.c

--- Day07/Ex00/ft_strdup.c
+++ Day07/Ex00/ft_strdup.c
@@ -2,25 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-//create enough memory equal to string1, copy the string1 to the pointer returned by malloc
-
 char *ft_strdup(char *src)
 {
   char* str;
   str = malloc(sizeof(char) * strlen(src) + 1);
-  if(str == 0)
+  if(str == NULL)
     {
-      return 0;
+      return NULL;
     }
   int i;
   i = 0;
-  while(src[i] != 0)
+  while(src[i] != '\0')
     {
       str[i] = src[i];
       i++;
     }
   str[i] = '\0';
-  free(str);
   return str;
 }
 
