2ebee01ed0c8c20274947e01fce3114d8e7640f4
robert ugarte <robertu@roberts-MBP.lan1>
2020-04-04 17:19:28 -0500

    ft_iterative_power.c

--- Day04/Ex02/ft_iterative_power.c
+++ Day04/Ex02/ft_iterative_power.c
@@ -4,6 +4,8 @@ int ft_iterative_power(int nb, int power)
 {
   int x = 1;
   int i = 0;
+  if (i > power)
+    return 0;
   while (i < power)
     {
       x = x*nb;
@@ -19,3 +21,5 @@ int main(void)
   printf("%d\n", ft_iterative_power(nb, power));
   return 0;
 }
+
+//
