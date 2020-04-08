630b16e998ce8bee8218d2fd39e68e6b91ad64a3
robert ugarte <robertu@roberts-MBP.lan1>
2020-04-08 03:11:05 -0500

    ft.h

---
+++ Day08/Ex01/ft.h
@@ -0,0 +1,12 @@
+#ifndef _FT_H
+#define _FT_H
+
+int ft_atoi(char *str);
+void ft_putchar(char c);
+int ft_max(int *arr, int len);
+void ft_putnbr(int nb);
+void ft_putstr(char *str);
+char *ft_strdup(char *src);
+int ft_strlen(char *str);
+
+#endif
