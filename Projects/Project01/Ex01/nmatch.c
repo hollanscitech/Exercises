9916e732f04dd3e55a2f0782a483a2e713730a2d
robert ugarte <robertu@roberts-MBP.lan1>
2020-05-07 10:49:37 -0500

    nmatch.c

--- Projects/Project01/Ex01/nmatch.c
+++ Projects/Project01/Ex01/nmatch.c
@@ -1,49 +1,28 @@
-//
 #include <stdio.h>
 
-int nmatch(char *s1, char *s2)
-{
-  if (*s2 == '\0')
-    {
-      if(*s1 != '\0')
-	{
-	  return 0;
-	}
-      else
-	{
-	  return 1;
-	}
-    }
-  if (*s2 == '*')
-    {
-      char *rest = s1;
-      int count = 0;
-      while (*rest != '\0')
-	{
-	  count = count + nmatch(rest, s2 + 1);
-	  rest++;
-	}
+int nmatch(char *s1, char *s2) {
+  if (*s2 == '\0') {
+    if(*s1 != '\0') {
+      return 0; }
+    else {
+      return 1; } }
+  if (*s2 == '*') {
+    char *rest = s1;
+    int count = 0;
+    while (*rest != '\0') {
       count = count + nmatch(rest, s2 + 1);
-      return count;
-    }
-  if (*s1 == '\0')
-    {
-      return 0;
-    }
-  if (*s2 == *s1)
-    {
-      return nmatch(s1 + 1, s2 + 1);
-    }
-  else
-    {
-      return 0;
-    }
-}
+      rest++; }
+    count = count + nmatch(rest, s2 + 1);
+    return count; }
+  if (*s1 == '\0') {
+    return 0; }
+  if (*s2 == *s1) {
+    return nmatch(s1 + 1, s2 + 1); }
+  else {
+    return 0; } }
 
-int main(void)
-{
+int main(void) {
   char *s1 = "abc";
   char *s2 = "a**";
   int output = nmatch(s1, s2);
-  printf("%d\n", output);
-}
+  printf("%d\n", output); }
