94489ee3483c358c1f5028f684926e1fcd586e46
robert ugarte <robertu@roberts-MBP.lan1>
2020-05-14 11:20:11 -0500

    nmatch.c

--- Projects/Project01/Ex01/nmatch.c
+++ Projects/Project01/Ex01/nmatch.c
@@ -1,125 +1,122 @@
 #include <stdio.h>
-#include <string.h>
 
-//if s2 has no star chars
-int nmatch1(char *s1, char *s2) {
-  int i = 0;
-  int x = 0;
-  int counter = 0;
-  while (s2[x] != '\0') {
-    i = 0;
-    while (s1[i] != '\0') {
-      if (s2[x] == s1[i]) {
-	counter++;
-	x++;
-      }
-      i++;
+int nmatch(char *s1, char *s2) {
+  if (*s2 == '\0') {
+    if(*s1 != '\0') {
+      return 0;
     }
-    x++;
-  }
-  return counter;
-}
- 
-//when s1 is 0 and s2 is only a star char
-int nmatch2(char *s1, char *s2) {
-  if (*s1 == 0 && *s2 == 0) {
-    return 1;
-  }
-  if (*s1 == 0 && strlen(s2) == 1 && *s2 == '*') {
-  return 1;
-  }
-  return 0;
-}
-
-//if s2 contains 2 or more star chars
-int nmatch3(char *s1, char *s2) {
-  int i = 0;
-  int counter = 0;
-  while (s1[i] != '\0' && s2[i] != '\0') {
-    if (s2[i] == '*' && (s2[i + 1] || s2[i - 1]) == (s1[i + 1] || s1[i - 1])) {
-      counter++;
+    else {
+      return 1;
     }
-    i++;
   }
-  return counter;
-}
-
-//if s2 contains 1 star   
-int nmatch4(char *s1, char *s2) {
-  int i = 0;
-  int counter = 0;
-  int len1 = strlen(s1);
-  while (s1[i] != '\0') {
-    if (s1[i] == ' ') {
-      counter++;
+  if (*s2 == '*') {
+    char *rest = s1;
+    int count = 0;
+    while (*rest != '\0') {
+      count = count + nmatch(rest, s2 + 1);
+      rest++;
     }
-    i++;
+    count = count + nmatch(rest, s2 + 1);
+    return count;
   }
-  if (len1 > counter && strlen(s2) == 1 && *s2 == '*') {
-    return 1;
+  if (*s1 == '\0') {
+    return 0;
   }
-  return 0;
+  if (*s2 == *s1) {
+    return nmatch(s1 + 1, s2 + 1);
+  }
+  else {
+    return 0;
+  }
 }
 
 void test(void) {
-  if (nmatch1("", " ") + nmatch2("", " ") + nmatch3("", " ") + nmatch4("", " ") == 0) {
-    printf("No Match (A)");
+  if (nmatch("hello", "hello") == 0) {
+    printf("0 Match (A)\n");
   }
-  if (nmatch1("", "") + nmatch2("", "") + nmatch3("", "") + nmatch4("", "") == 0) {
-    printf("No Match (B)");
+  if (nmatch("hello", "h") == 0) {
+    printf("0 Match (B)\n");
   }
-  if (nmatch1(" ", "*") + nmatch2(" ", "*") + nmatch3(" ", "*") + nmatch4(" ", "*") == 0) {
-    printf("No Match (C)");
+  if (nmatch("hello", "hell") == 0) {
+    printf("0 Match (C)\n");
   }
-  if (nmatch1("hello", "*") + nmatch2("hello", "*") + nmatch3("hello", "*") + nmatch4("hello", "*") == 0) {
-    printf("No Match (D)");
+  if (nmatch("", " ") == 0) {
+    printf("0 Match (D)\n");
   }
-  if (nmatch1("hello", "h") + nmatch2("hello", "h") + nmatch3("hello", "h") + nmatch4("hello", "h") == 0) {
-    printf("No Match (E)");
+  if (nmatch("", "") == 0) {
+    printf("0 Match (E)\n");
   }
-  if (nmatch1("hello", "ho") + nmatch2("hello", "ho") + nmatch3("hello", "ho") + nmatch4("hello", "ho") == 0) {
-    printf("No Match (F)");
+  if (nmatch(" ", "*") == 0) {
+    printf("0 Match (F)\n");
   }
-  if (nmatch1("hello", "h*") + nmatch2("hello", "h*") + nmatch3("hello", "h*") + nmatch4("hello", "h*") == 0) {
-    printf("No Match (G)");
+  if (nmatch("hello", "*") == 0) {
+    printf("0 Match (G)\n");
   }
-  if (nmatch1("hello", "hlo") + nmatch2("hello", "hlo") + nmatch3("hello", "hlo") + nmatch4("hello", "hlo") == 0) {
-    printf("No Match (H)");
+  if (nmatch("hello", "h") == 0) {
+    printf("0 Match (H)\n");
   }
-  if (nmatch1("hello", "hello") + nmatch2("hello", "hello") + nmatch3("hello", "hello") + nmatch4("hello", "hello") == 0) {
-    printf("No Match (I)");
+  if (nmatch("hello", "he") == 0) {
+    printf("0 Match (I)\n");
   }
-  if (nmatch1("hello", "hell*") + nmatch2("hello", "hell*") + nmatch3("hello", "hell*") + nmatch4("hello", "hell*") == 0) {
-    printf("No Match (J)");
+  if (nmatch("hello", "h*") == 0) {
+    printf("0 Match (J)\n");
   }
-  if (nmatch1("hello", "hello*") + nmatch2("hello", "hello*") + nmatch3("hello", "hello*") + nmatch4("hello", "hello*") == 0) {
-    printf("No Match (K)");
+  if (nmatch("hello", "hlo") == 0) {
+    printf("0 Match (K)\n");
   }
-  if (nmatch1("hello", "he*") + nmatch2("hello", "he*") + nmatch3("hello", "he*") + nmatch4("hello", "he*") == 0) {
-    printf("No Match (L)");
+  if (nmatch("hello", "hell*") == 0) {
+    printf("0 Match (L)\n");
   }
-  if (nmatch1("hello", "lo") + nmatch2("hello", "lo") + nmatch3("hello", "lo") + nmatch4("hello", "lo") == 0) {
-    printf("No Match (M)");
+  if (nmatch("hello", "hello*") == 0) {
+    printf("0 Match (M)\n");
+  }
+  if (nmatch("hello", "he*") == 0) {
+    printf("0 Match (N)\n");
+  }
+  if (nmatch("hello", "lo") == 0) {
+    printf("0 Match (O)\n");
+  }
+  if (nmatch("abcbd", "*b*") == 0) {
+    printf("0 Match (P)\n");
+  }
+  if (nmatch("abc", "a**") == 0) {
+    printf("0 Match (Q)\n");
+  }
+  if (nmatch("abc", "abc") == 0) {
+    printf("0 Match (R)\n");
+  }
+  if (nmatch("abc", "ab*") == 0) {
+    printf("0 Match (S)\n");
+  }
+  if (nmatch("abc", "abc*") == 0) {
+    printf("0 Match (T)\n");
+  }
+  if (nmatch("abc", "*") == 0) {
+    printf("0 Match (U)\n");
+  }
+  if (nmatch("abc", "**") == 0) {
+    printf("0 Match (V)\n");
+  }
+  if (nmatch("abc", "***") == 0) {
+    printf("0 Match (W)\n");
+  }
+  if (nmatch("abc", "**a") == 0) {
+    printf("0 Match (X)\n");
+  }
+  if (nmatch("abc", "*a*") == 0) {
+    printf("0 Match (Y)\n");
+  }
+  if (nmatch("abc", "cba") == 0) {
+    printf("0 Match (Z)\n");
   }
 }
 
 int main(int argc, char *argv[]) {
   test();
   if (argc == 3) {
-    int count1 = nmatch1(argv[1], argv[2]);
-    int count2 = nmatch2(argv[1], argv[2]);
-    int count3 = nmatch3(argv[1], argv[2]);
-    int count4 = nmatch4(argv[1], argv[2]);
-    int total = count1 + count2 + count3 + count4;
-    printf("%d\n", total);
+    char *s1 = argv[1];
+    char *s2 = argv[2];
+    int output = nmatch(s1, s2);
+    printf("%d\n", output);
   }
-  return 0;
 }
-
-/*int main(int argc, char *argv[]) {
-  if (argc == 3) {
-    int count = nmatch2(argv[1], argv[2]) + nmatch3(argv[1], argv[2]) + nmatch4(argv[1], argv[2]) + nmatch5(argv[1], argv[2]);
-    printf("%d\n", count);
-  }
-  return 0;
-  }*/
